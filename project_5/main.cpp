e1e9586b01116b03c73a40e8a19d439271d999fe
Leison Gao <leisongao2005@g.ucla.edu>
2023-11-13 11:17:16 -0800

    start

--- project_5/main.cpp
+++ project_5/main.cpp
@@ -6,9 +6,42 @@
 //
 
 #include <iostream>
+#include <fstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+
+int main() {
+    // output code
+    ofstream outfile("/Users/leisongao/desktop/cs31/Projects/project_5/textFiles/results.text");
+    // outfile is a name of our choosing.
+    if ( ! outfile )// Did the creation fail?
+    {
+        cerr << "Error: Cannot create results.txt!" << endl;
+    }
+    outfile << "This will be written to the file" << endl;
+    outfile << "2 + 2 = " << 2+2 << endl;
     return 0;
+    
+    // input code
+    ifstream infile("data.txt");    // infile is a name of our choosing
+    if ( ! infile )                // Did opening the file fail?
+    {
+        cerr << "Error: Cannot open data.txt!" << endl;
+    }
+    
+    // reading char
+    char c;
+    infile.get(c);
+    
+    // reading line by line
+    const int MAX = ....;
+    char line[MAX];
+    infile.getline(line, MAX);
+    
+    // reading entire file
+    while ( infile.getline(line, MAX) )
+    {
+        ... process line
+    }
 }
